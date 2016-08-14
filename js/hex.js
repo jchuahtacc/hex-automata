1b06264419399e28488ad5715083d65d52b13ec7
jychuah <jychuah@gmail.com>
2016-08-13 23:37:30 -0500

    Click events and regions

--- js/hex.js
+++ js/hex.js
@@ -1,20 +1,26 @@
-var grid = { };
-var dRow = 120;
-var dColX = 180;
-var dColY = 180;
-var offsetX = 0;
-var offsetY = 0;
 
 var Hex = function(row, col) {
     this.row = row;
     this.col = col;
+    this.clickCount = 0;
+    Hex.prototype.click = function() {
+        console.log("click", this);
+    }
+    Hex.prototype.edgeClick = function(degree) {
+        console.log(degree + " degree click", this);
+    }
 }
 
 var HexMap = function(svgSelector) {
     var radius = 60;
     var svg = d3.select(svgSelector);
     var hexString = "";
-
+    var dRow = 120;
+    var dColX = 180;
+    var dColY = 180;
+    var offsetX = 0;
+    var offsetY = 0;
+    var edgeWidth = 10;
  
     function buildHexString() {
         var height = radius / 2 * Math.sqrt(3);
@@ -26,7 +32,6 @@ var HexMap = function(svgSelector) {
         var translate = "translate(" + Math.floor(d.col * dColX + offsetX) + " " + Math.floor(d.row * dRow + d.col * dColY + offsetY) + ")";
         return translate;
     }
-
 
     HexMap.prototype.map = { };
 
@@ -52,6 +57,11 @@ var HexMap = function(svgSelector) {
         this.draw();
     }
 
+    HexMap.prototype.setEdgeWidth = function(edgeWidth) {
+        this.edgeWidth = edgeWidth;
+        this.draw();
+    }
+
     HexMap.prototype.buildRect = function(rows, cols) {
         for (var i = 0; i < rows; i++) {
             for (var j = 0; j < (i + 1) * 2 - 1 && j < cols; j++) {
@@ -74,26 +84,79 @@ var HexMap = function(svgSelector) {
         this.draw();
     }
 
+    function makeTrapezoids() {
+        var traps = [ ];
+        if (radius > 15) {
+            var rad = radius - edgeWidth;
+            for (var deg = 0; deg < 360; deg = deg + 60) {
+                var string = "M ";
+                var cos = Math.cos(deg * Math.PI / 180);
+                var sin = -Math.sin(deg * Math.PI / 180);
+                string += Math.floor(cos * rad) + " " + Math.floor(sin * rad);
+                string += " L " + Math.floor(cos * radius) + " " + Math.floor(sin * radius);
+                var cos2 = Math.cos((deg + 60) * Math.PI / 180);
+                var sin2 = -Math.sin((deg + 60) * Math.PI / 180);
+                string += " L " + Math.floor(cos2 * radius) + " " + Math.floor(sin2 * radius);
+                string += " L " + Math.floor(cos2 * rad) + " " + Math.floor(sin2 * rad) + " Z ";
+                traps.push(string);
+            }
+        }
+        return traps;
+    }
+
+    function makeInnerHex() {
+        var rad = radius - edgeWidth;
+        var string = "M " + rad + " 0 ";
+        for (var deg = 60; deg < 360; deg = deg + 60) {
+            string += "L " + Math.floor(Math.cos(deg * Math.PI / 180) * rad) + " " + Math.floor(Math.sin(deg * Math.PI / 180) * -rad) + " ";
+        }
+        string += "Z";
+        return string;
+    }
+
     HexMap.prototype.draw = function() {
         svg.select("path").remove();
-        svg.selectAll("path")
+        var groups = svg.selectAll("path")
             .data(this.toArray())
             .enter()
-            .append("path")
+            .append("g");
+        groups.append("path")
             .attr("row", function(d) { return d.row; })
             .attr("col", function(d) { return d.col; })
             .attr("d", hexString)
             .attr("transform", translate)
             .attr("stroke", "black")
             .attr("fill", "none")
-            .attr("stroke-width", 3);
+            .attr("stroke-width", 3)
+            .attr("pointer-events", "visible");
+        var traps = makeTrapezoids();
+        var innerHex = makeInnerHex();
+        for (var index in traps) {
+            groups.append("path")
+                .attr("d", traps[index])
+                .attr("transform", translate)
+                .attr("stroke", "none")
+                .attr("pointer-events", "visible")
+                .attr("fill", "none")
+                .attr("degree", function (d) { return Math.floor(index * 60 + 30); })
+                .on("click", function(d, i) { d.edgeClick.apply(d, [Math.floor(parseInt(this.getAttribute("degree")))]); }); 
+        }
+        groups.append("path")
+            .attr("d", innerHex)
+            .attr("transform", translate)
+            .attr("stroke", "none")
+            .attr("pointer-events", "visible")
+            .attr("fill", "none")
+            .on("click", function(d, i) { d.click.apply(d); });
     }
 
     this.setRadius(60);
 }
 
+var map = null;
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    var map = new HexMap("#hexmap");
+    map = new HexMap("#hexmap");
     d3.select("#hexmap").attr("width", 600).attr("height", 600);
     map.buildRect(5, 5);
 });
